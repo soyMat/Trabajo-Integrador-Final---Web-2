fd4cb926e7a242d35ba1a5d2544b1c4c3eb1199f
soyMat <mazuelamateo16@gmail.com>
2025-11-29 22:57:15 -0300

    Implementacion de un background de la libreria React Bits

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PaginaInicio from './paginas/PaginaInicio.jsx'
+import Beams from './componentes/Beams.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PaginaInicio />
+    {/* Contenedor general de la app */}
+    <div className="relative min-h-screen text-white overflow-hidden">
+      
+      {/* Fondo Beams */}
+      <div className="fixed inset-0 -z-10 pointer-events-none">
+        <Beams
+          beamWidth={2}
+          beamHeight={15}
+          beamNumber={12}
+          lightColor="#ffffff"
+          speed={2}
+          noiseIntensity={1.75}
+          scale={0.2}
+          rotation={0}
+        />
+      </div>
+
+      {/* App */}
+      <div className="relative z-10">
+        <PaginaInicio />
+      </div>
+    </div>
   </React.StrictMode>,
-) 
+  {/* cd "E:\universidad\web\TRABAJO FINAL WEB\vite-project" */}
+)
